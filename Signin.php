eeb905676e1b7a1c56473582aa41731c8749de47
Chaitra Bhat <chaitrabhat084@gmail.com>
2022-11-18 15:06:33 +0530

    Added Files

--- Signin.php
+++ Signin.php
@@ -6,9 +6,9 @@
     <title>Gabage Collection Management</title>
 
     <!-- Site favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="vendors/images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="vendors/images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="vendors/images/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="vendors/images/waste.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="vendors/images/waste.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="vendors/images/waste.png">
 
     <!-- Mobile Specific Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -17,7 +17,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
-    <link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
     <link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -26,10 +25,7 @@
 <body class="login-page">
     <div class="login-header box-shadow">
         <div class="container-fluid d-flex justify-content-between align-items-center">
-            <div class="brand-logo">
-                <a href="signin.php">
-                    <img src="vendors/images/deskapp-logo.png" alt="">
-                </a>
+            <div class="brand-logo" >
             </div>
             <!-- <div class="login-menu">
                 
@@ -41,7 +37,7 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-6 col-lg-7">
-                    <img src="vendors/images/login-page-img.png" alt="">
+                    <img src="vendors/images/waste.png" alt="" width="700" height="600">
                 </div>
                 <div class="col-md-6 col-lg-5">
                      
@@ -73,9 +69,7 @@
                                         <label class="custom-control-label" for="customCheck1">Remember</label>
                                     </div>
                                 </div>
-                                <div class="col-6">
-                                    <div class="forgot-password"><a href="forgot-password.php">Forgot Password?</a></div>
-                                </div>
+                                
                             </div>
                             <div class="row">
                                 <div class="col-sm-12">
